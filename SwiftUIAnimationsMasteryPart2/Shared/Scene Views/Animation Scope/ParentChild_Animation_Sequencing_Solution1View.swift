80caa55384176560b438279f06d86f3242de9b77
CypherPoet <CypherPoet@gmail.com>
2021-02-06 14:34:56 -0600

    ParentChild_Animation Solution Examples

--- SwiftUIAnimationsMasteryPart2/Shared/Scene Views/Animation Scope/ParentChild_Animation_Sequencing_Solution1View.swift
+++ SwiftUIAnimationsMasteryPart2/Shared/Scene Views/Animation Scope/ParentChild_Animation_Sequencing_Solution1View.swift
@@ -1,5 +1,5 @@
 //
-// ParentChild_Animation_Sequencing_ProblemExampleView.swift
+// ParentChild_Animation_Sequencing_Solution1View.swift
 // SwiftUIAnimationsMasteryPart2
 //
 // Created by CypherPoet on 2/3/21.
@@ -9,22 +9,25 @@
 import SwiftUI
 
 
-struct ParentChild_Animation_Sequencing_ProblemExampleView {
+struct ParentChild_Animation_Sequencing_Solution1View {
     @State private var isInfoPanelExpanded = false
+    @State private var isPanelContentVisible = false
 }
 
 
 
 // MARK: - `View` Body
-extension ParentChild_Animation_Sequencing_ProblemExampleView: View {
+extension ParentChild_Animation_Sequencing_Solution1View: View {
     
     var body: some View {
-        VStack {
+        VStack(spacing: 18.0) {
             Text("Sequencing Parent and Child Views")
                 .screenBanner(backgroundColor: .accentColor)
+
+            Text("Solution 1")
+                .font(.title2)
             
             ScreenSectionDivider()
-                .padding(.vertical)
             
             VStack(spacing: 18.0) {
                 Text("""
@@ -36,8 +39,7 @@ extension ParentChild_Animation_Sequencing_ProblemExampleView: View {
                 ScreenSectionDivider()
                 
                 Text("""
-                What we want is for the view to slide up, and THEN have the child views appear. \
-                Look what happens when we try to accomplish without withAnimation.
+                What we want is for the view to slide up, and THEN have the child views appear.
                 """
                 )
                 
@@ -58,7 +60,7 @@ extension ParentChild_Animation_Sequencing_ProblemExampleView: View {
 
 
 // MARK: - Computeds
-extension ParentChild_Animation_Sequencing_ProblemExampleView {
+extension ParentChild_Animation_Sequencing_Solution1View {
     
     func panelOffset(in geometry: GeometryProxy) -> CGSize {
         let frame = geometry.frame(in: .global)
@@ -72,7 +74,7 @@ extension ParentChild_Animation_Sequencing_ProblemExampleView {
 
 
 // MARK: - View Content Builders
-private extension ParentChild_Animation_Sequencing_ProblemExampleView {
+private extension ParentChild_Animation_Sequencing_Solution1View {
     
     
     var infoPanel: some View {
@@ -88,10 +90,9 @@ private extension ParentChild_Animation_Sequencing_ProblemExampleView {
             infoPanelContent
                 .frame(maxWidth: .infinity)
                 .padding()
-                .background(RoundedRectangle(cornerRadius: 15).opacity(0.1))
+                .background(RoundedRectangle(cornerRadius: 15).opacity(0.11))
                 .padding(.horizontal)
-                .opacity(isInfoPanelExpanded ? 0.8 : 0.0)
-                .animation(Animation.easeInOut.delay(0.5))
+                .opacity(isPanelContentVisible ? 1 : 0.0)
         }
         .padding(.trailing)
         .background(
@@ -99,9 +100,14 @@ private extension ParentChild_Animation_Sequencing_ProblemExampleView {
                 .fill(Color.purple)
                 .shadow(radius: 8)
         )
-        .animation(.easeInOut)
         .onTapGesture {
-            isInfoPanelExpanded.toggle()
+            withAnimation(Animation.easeInOut) {
+                isInfoPanelExpanded.toggle()
+            }
+
+            withAnimation(Animation.easeInOut.delay(0.5)) {
+                isPanelContentVisible.toggle()
+            }
         }
     }
     
@@ -110,13 +116,7 @@ private extension ParentChild_Animation_Sequencing_ProblemExampleView {
         VStack(spacing: 10) {
             Image(systemName: "slider.horizontal.3").font(.largeTitle)
             
-            Text("""
-            We want this to JUST fade in after the parent is done animating out.
-
-            Instead, the animation delay causes the offset change to be triggered \
-            too late, and it ends up running alongside the opacity change.
-            """
-            )
+            Text("Much better 😻")
             
             Spacer()
         }
@@ -125,17 +125,17 @@ private extension ParentChild_Animation_Sequencing_ProblemExampleView {
 
 
 // MARK: - Private Helpers
-private extension ParentChild_Animation_Sequencing_ProblemExampleView {
+private extension ParentChild_Animation_Sequencing_Solution1View {
 }
 
 
 #if DEBUG
 // MARK: - Preview
-struct ParentChild_Animation_Sequencing_ProblemExampleView_Previews: PreviewProvider {
+struct ParentChild_Animation_Sequencing_Solution1View_Previews: PreviewProvider {
     
     static var previews: some View {
         NavigationView {
-            ParentChild_Animation_Sequencing_ProblemExampleView()
+            ParentChild_Animation_Sequencing_Solution1View()
         }
     }
 }
--- SwiftUIAnimationsMasteryPart2/Shared/Scene Views/Animation Scope/ParentChild_Animation_Sequencing_Solution2View.swift
+++ SwiftUIAnimationsMasteryPart2/Shared/Scene Views/Animation Scope/ParentChild_Animation_Sequencing_Solution2View.swift
@@ -1,5 +1,5 @@
 //
-// ParentChild_Animation_Sequencing_ProblemExampleView.swift
+// ParentChild_Animation_Sequencing_Solution2View.swift
 // SwiftUIAnimationsMasteryPart2
 //
 // Created by CypherPoet on 2/3/21.
@@ -9,22 +9,30 @@
 import SwiftUI
 
 
-struct ParentChild_Animation_Sequencing_ProblemExampleView {
+struct ParentChild_Animation_Sequencing_Solution2View {
     @State private var isInfoPanelExpanded = false
+    @State private var isPanelContentVisible = false
 }
 
 
 
 // MARK: - `View` Body
-extension ParentChild_Animation_Sequencing_ProblemExampleView: View {
+extension ParentChild_Animation_Sequencing_Solution2View: View {
     
     var body: some View {
-        VStack {
+        VStack(spacing: 18.0) {
             Text("Sequencing Parent and Child Views")
                 .screenBanner(backgroundColor: .accentColor)
+
+            VStack {
+                Text("Solution 2")
+                    .font(.title2)
+                
+                Text("Using Transitions")
+                    .font(.subheadline)
+            }
             
             ScreenSectionDivider()
-                .padding(.vertical)
             
             VStack(spacing: 18.0) {
                 Text("""
@@ -36,8 +44,7 @@ extension ParentChild_Animation_Sequencing_ProblemExampleView: View {
                 ScreenSectionDivider()
                 
                 Text("""
-                What we want is for the view to slide up, and THEN have the child views appear. \
-                Look what happens when we try to accomplish without withAnimation.
+                What we want is for the view to slide up, and THEN have the child views appear.
                 """
                 )
                 
@@ -58,7 +65,7 @@ extension ParentChild_Animation_Sequencing_ProblemExampleView: View {
 
 
 // MARK: - Computeds
-extension ParentChild_Animation_Sequencing_ProblemExampleView {
+extension ParentChild_Animation_Sequencing_Solution2View {
     
     func panelOffset(in geometry: GeometryProxy) -> CGSize {
         let frame = geometry.frame(in: .global)
@@ -72,7 +79,7 @@ extension ParentChild_Animation_Sequencing_ProblemExampleView {
 
 
 // MARK: - View Content Builders
-private extension ParentChild_Animation_Sequencing_ProblemExampleView {
+private extension ParentChild_Animation_Sequencing_Solution2View {
     
     
     var infoPanel: some View {
@@ -85,23 +92,37 @@ private extension ParentChild_Animation_Sequencing_ProblemExampleView {
             .font(.title)
             .padding()
             
-            infoPanelContent
-                .frame(maxWidth: .infinity)
-                .padding()
-                .background(RoundedRectangle(cornerRadius: 15).opacity(0.1))
-                .padding(.horizontal)
-                .opacity(isInfoPanelExpanded ? 0.8 : 0.0)
-                .animation(Animation.easeInOut.delay(0.5))
+            if isInfoPanelExpanded {
+                infoPanelContent
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(RoundedRectangle(cornerRadius: 15).opacity(0.11))
+                    .padding(.horizontal)
+                    .transition(
+                        .asymmetric(
+                            insertion: AnyTransition.opacity.animation(Animation.easeInOut.delay(0.5)),
+                            removal: AnyTransition.identity.animation(.easeInOut)
+                        )
+                    )
+            }
+            
+            Color.clear
         }
+        .frame(maxWidth: .infinity)
         .padding(.trailing)
         .background(
             RoundedRectangle(cornerRadius: 20)
                 .fill(Color.purple)
                 .shadow(radius: 8)
         )
-        .animation(.easeInOut)
         .onTapGesture {
-            isInfoPanelExpanded.toggle()
+            withAnimation(Animation.easeInOut) {
+                isInfoPanelExpanded.toggle()
+            }
+//
+//            withAnimation(Animation.easeInOut.delay(0.5)) {
+//                isPanelContentVisible.toggle()
+//            }
         }
     }
     
@@ -109,33 +130,24 @@ private extension ParentChild_Animation_Sequencing_ProblemExampleView {
     var infoPanelContent: some View {
         VStack(spacing: 10) {
             Image(systemName: "slider.horizontal.3").font(.largeTitle)
-            
-            Text("""
-            We want this to JUST fade in after the parent is done animating out.
-
-            Instead, the animation delay causes the offset change to be triggered \
-            too late, and it ends up running alongside the opacity change.
-            """
-            )
-            
-            Spacer()
+            Text("Much better 😻")
         }
     }
 }
 
 
 // MARK: - Private Helpers
-private extension ParentChild_Animation_Sequencing_ProblemExampleView {
+private extension ParentChild_Animation_Sequencing_Solution2View {
 }
 
 
 #if DEBUG
 // MARK: - Preview
-struct ParentChild_Animation_Sequencing_ProblemExampleView_Previews: PreviewProvider {
+struct ParentChild_Animation_Sequencing_Solution2View_Previews: PreviewProvider {
     
     static var previews: some View {
         NavigationView {
-            ParentChild_Animation_Sequencing_ProblemExampleView()
+            ParentChild_Animation_Sequencing_Solution2View()
         }
     }
 }
